ecd7e5f6829ba57087556bc4c7775fc722fb9263
西瓜榴莲鸡 <40055324+rw98317@users.noreply.github.com>
2018-06-12 13:49:18 +0800

    HelloWorld.java
    this is a sample program

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld
+{
+private String text;
+
+public HelloWorld(String text)
+{
+this.text=text;
+}
+public static void main(String []args)
+{
+
+System.out.println(text);
+
+}
+
+
+}
